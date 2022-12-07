b8ecf43e402a18d63a26e6affa47a872fe93925e
andrei-dascalu3 <andrei.dascalu2@gmail.com>
2022-12-07 18:14:43 +0200

    Added REST Configuration class

---
+++ src/main/java/com/fii/backendapp/configuration/RestConfiguration.java
@@ -0,0 +1,17 @@
+package com.fii.backendapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.fii.backendapp.entity.User;
+
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(User.class);
+    }
+}
